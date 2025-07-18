9fda72f24a72ff5686375c83fc0b9f7308dc6330
Gabor Plesz <gabor.plesz@gmail.com>
2025-07-18 19:33:36 +0200

    megjegyzések törlése

--- Program.cs
+++ Program.cs
@@ -1,27 +1,4 @@
-﻿// Red Bull	    AUT Ausztria	GBR Milton Keynes
-// Ferrari	    ITA Olaszország	ITA Maranello
-// Mercedes	    GER Németország	GBR Brackley
-// Alpine	    FRA Franciaország	GBR Enstone
-// McLaren	    GBR Egyesült Királyság	GBR Woking
-// Alfa Romeo	SUI Svájc	SUI Hinwil
-// Aston Martin	GBR Egyesült Királyság	GBR Silverstone
-// Haas	        USA Egyesült Államok	USA Kannapolis
-// AlphaTauri	ITA Olaszország	ITA Faenza
-// Williams	    GBR Egyesült Királyság	GBR Oxfordshire
-// Console.WriteLine("ide jön a szöveg");
-
-// Console.WriteLine("Red Bull");
-// Console.WriteLine("Ferrari"); 
-// Console.WriteLine("Mercedes");	   
-// Console.WriteLine("Alpine");	   
-// Console.WriteLine("McLaren");	   
-// Console.WriteLine("Alfa Romeo");	
-// Console.WriteLine("Aston Martin");
-// Console.WriteLine("Haas");	       
-// Console.WriteLine("AlphaTauri");
-// Console.WriteLine("Williams");  	 
-
-using Microsoft.VisualBasic;
+﻿using Microsoft.VisualBasic;
 
 var list = new string[] {
     "Red Bull",
