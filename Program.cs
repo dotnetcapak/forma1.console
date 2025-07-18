edccc1149b47ebb8f071904ba550b72b86da2d0e
Gabor Plesz <gabor.plesz@gmail.com>
2025-07-18 19:12:08 +0200

    csapatnevek megjegyzésbe téve

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
-﻿Red Bull	    AUT Ausztria	GBR Milton Keynes
-Ferrari	    ITA Olaszország	ITA Maranello
-Mercedes	    GER Németország	GBR Brackley
-Alpine	    FRA Franciaország	GBR Enstone
-McLaren	    GBR Egyesült Királyság	GBR Woking
-Alfa Romeo	SUI Svájc	SUI Hinwil
-Aston Martin	GBR Egyesült Királyság	GBR Silverstone
-Haas	        USA Egyesült Államok	USA Kannapolis
-AlphaTauri	ITA Olaszország	ITA Faenza
-Williams	    GBR Egyesült Királyság	GBR Oxfordshire
-Console.WriteLine("ide jön a szöveg");
+﻿// Red Bull	    AUT Ausztria	GBR Milton Keynes
+// Ferrari	    ITA Olaszország	ITA Maranello
+// Mercedes	    GER Németország	GBR Brackley
+// Alpine	    FRA Franciaország	GBR Enstone
+// McLaren	    GBR Egyesült Királyság	GBR Woking
+// Alfa Romeo	SUI Svájc	SUI Hinwil
+// Aston Martin	GBR Egyesült Királyság	GBR Silverstone
+// Haas	        USA Egyesült Államok	USA Kannapolis
+// AlphaTauri	ITA Olaszország	ITA Faenza
+// Williams	    GBR Egyesült Királyság	GBR Oxfordshire
+// Console.WriteLine("ide jön a szöveg");
 
 
 
