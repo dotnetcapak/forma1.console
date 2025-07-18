1811d650f9a2ff8b0342718ab3c0f30840645176
Gabor Plesz <gabor.plesz@gmail.com>
2025-07-18 19:10:12 +0200

    csapatnevek az internetről nyers állapotba

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Red Bull	    AUT Ausztria	GBR Milton Keynes
+Ferrari	    ITA Olaszország	ITA Maranello
+Mercedes	    GER Németország	GBR Brackley
+Alpine	    FRA Franciaország	GBR Enstone
+McLaren	    GBR Egyesült Királyság	GBR Woking
+Alfa Romeo	SUI Svájc	SUI Hinwil
+Aston Martin	GBR Egyesült Királyság	GBR Silverstone
+Haas	        USA Egyesült Államok	USA Kannapolis
+AlphaTauri	ITA Olaszország	ITA Faenza
+Williams	    GBR Egyesült Királyság	GBR Oxfordshire
+Console.WriteLine("ide jön a szöveg");
 
 
 
